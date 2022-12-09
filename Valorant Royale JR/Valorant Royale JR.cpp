ddad1f1212c723f44065fbd7f5be0e876a2b4a97
Joel Roiz <Joel Roiz@DESKTOP-K6B4IDO>
2022-12-08 23:57:21 -0700

    Agregar archivos de proyecto.

---
+++ Valorant Royale JR/Valorant Royale JR.cpp
@@ -0,0 +1,528 @@
+// battle royal.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&(((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%(((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#(((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((((((((@@@@@@@@@@@@@@@@@@@((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((((((((@@@@@@@@@@@@@@@((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((((((((@@@@@@@@@@@((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((((((((@@@@@@@((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+
+    cout << "((((@@@@@@@((((@@@@@(((((@@@(((@@@@@@@@@@((((((((((((@@@@@@(((((((((((((@@@@(((((@@@@@@@@(((((@@@@@@@@(((@@(((((((((((((" << endl;
+    Sleep(100);
+    cout << "((((@@@@@@@((((@@@@@(((((@@@(((@@@@@@@@@@((((((((((((@@@@@@(((((((((((((@@@@(((((@@@@@@@@(((((@@@@@@@@(((@@(((((((((((((" << endl;
+    Sleep(100);
+    cout << "((((@@@@@((((@@@@@(((((((@@@(((@@@@@@@@((((@@@@@@@@((((@@@@(((@@@@@((((%@@@@(((((((@@@@@@(((((((@@@@@@(((@@@@@@@(((@@@@@" << endl;
+    Sleep(100);
+    cout << "((((@@@((((@@@@@((((@@(((@@@(((@@@@@@@@(((@@@@@@@@@@@(((@@@(((@@@((((%@@@@@@(((@@((((@@@@((((@((((@@@@(((@@@@@@@(((@@@@@" << endl;
+    Sleep(100);
+    cout << "((((@((((@@@@@((((((@@(((@@@(((@@@@@@@@(((@@@@@@@@@@@(((@@@(((@@((((@@@@@@@@(((@@((((((@@@&((@@@((((@@(((@@@@@@@(((@@@@@" << endl;
+    Sleep(100);
+    cout << "(((((((@@@@@((((@@(((((((@@@(((@@@@@@@@/(((@@@@@@@@((((@@@@(((@@@@((((@@@@@@(((((((@@((((@@@@@@@@@(((((((@@@@@@@(((@@@@@" << endl;
+    Sleep(100);
+    cout << "(((((@@@@@((((@@@@@@(((((@@@((((((((((@@@/(((((((((((@@@@@@(((@@@@@@((((@@@@(((((@@@@@@((((@@@@@@@@@(((((@@@@@@@(((@@@@@" << endl;
+    Sleep(100);
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    Sleep(100);
+
+    int pistola = 0;
+    string nickname;
+    int jugador = 0;
+    int jugador2 = 0;
+    int jugador3 = 0;
+    int jugador4 = 0;
+    int jugador5 = 0;
+    int jugador6 = 0;
+    int jugador7 = 0;
+    int jugador8 = 0;
+    int jugador9 = 0;
+    int jugador10 = 0;
+    int jugador11 = 0;
+    int jugador12 = 0;
+    int jugador13 = 0;
+    int jugador14 = 0;
+    int jugador15 = 0;
+    int jugador16 = 0;
+    int jugador17 = 0;
+    int jugador18 = 0;
+    int jugador19 = 0;
+    int jugador20 = 0;
+    int jugador21 = 0;
+    int jugador22 = 0;
+
+    srand(time(NULL));
+    jugador2 = 800 + rand() % 100;
+    jugador3 = 800 + rand() % 100;
+    jugador4 = 800 + rand() % 100;
+    jugador5 = 800 + rand() % 100;
+    jugador6 = 800 + rand() % 100;
+    jugador7 = 800 + rand() % 100;
+    jugador8 = 800 + rand() % 100;
+    jugador9 = 800 + rand() % 100;
+    jugador10 = 800 + rand() % 135;
+    jugador11 = 800 + rand() % 135;
+    jugador12 = 800 + rand() % 135;
+    jugador13 = 800 + rand() % 135;
+    jugador14 = 800 + rand() % 135;
+    jugador15 = 800 + rand() % 135;
+    jugador16 = 800 + rand() % 135;
+    jugador17 = 800 + rand() % 135;
+    jugador18 = 800 + rand() % 135;
+    jugador19 = 800 + rand() % 135;
+    jugador20 = 800 + rand() % 135;
+    jugador21 = 800 + rand() % 135;
+    jugador22 = 800 + rand() % 135;
+    ;
+    cout << "bienvenido a valorant que gane el mejor radiante" << endl;
+    cout << "ingresta tu nickname" << endl;
+    cin >> nickname;
+    cout << "bienvenido " << nickname << endl;
+    cout << "selecciona tu arma con el numero,\n recuerda que cada arma te dara diferente daño " << endl;
+    cout << "1: vandal. 2:phantom. 3- operator. randome - cuchillo ";
+    cin >> pistola;
+    switch (pistola)
+    {
+    case 1: jugador = 850;
+        break;
+    case 2: jugador = 900;
+        break;
+    case 3: jugador = 950;
+
+    default:
+        jugador + 100;
+        break;
+    }
+    ////////////////////////////////////////////////////
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Jett " << endl;
+    Sleep(500);
+    system("cls");
+
+    if (jugador > jugador2)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador2 << endl;
+        return(0);
+    }
+
+    ////////////////////////////////////////////////////
+
+
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Reyna " << endl;
+    system("cls");
+
+    if (jugador > jugador3)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador3 << endl;
+        return(0);
+    }
+
+    ////////////////////////////////////////////////////
+    system("cls");
+
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Phoenix " << endl;
+    system("cls");
+
+    if (jugador > jugador4)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador4 << endl;
+        return(0);
+    }
+    ////////////////////////////////////////////////////
+
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Raze " << endl;
+    system("cls");
+
+    if (jugador > jugador5)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador5 << endl;
+        return(0);
+    }
+    ////////////////////////////////////////////////////
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Yoru " << endl;
+    system("cls");
+
+    if (jugador > jugador6)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador6 << endl;
+        return(0);
+    }
+    ////////////////////////////////////////////////////
+    system("cls");
+
+
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Neon " << endl;
+    system("cls");
+
+    if (jugador > jugador7)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador7 << endl;
+        return(0);
+    }
+    ////////////////////////////////////////////////////
+
+    system("cls");
+
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Chamber " << endl;
+    system("cls");
+
+    if (jugador > jugador8)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador8 << endl;
+        return(0);
+    }
+
+    ////////////////////////////////////////////////////
+
+
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Cypher " << endl;
+    system("cls");
+
+    if (jugador > jugador9)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador9 << endl;
+        return(0);
+    }
+
+    ////////////////////////////////////////////////////
+
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Killjoy " << endl;
+    system("cls");
+
+    if (jugador > jugador10)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador10 << endl;
+        return(0);
+    }
+
+    ////////////////////////////////////////////////////
+
+
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Sage " << endl;
+    system("cls");
+
+    if (jugador > jugador11)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador11 << endl;
+        return(0);
+    }
+
+    ////////////////////////////////////////////////////
+
+
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Sova " << endl;
+    system("cls");
+
+    if (jugador > jugador12)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador12 << endl;
+        return(0);
+    }
+
+    ////////////////////////////////////////////////////
+
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Fade " << endl;
+    system("cls");
+
+    if (jugador > jugador13)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador13 << endl;
+        return(0);
+    }
+    ////////////////////////////////////////////////////
+
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Breach " << endl;
+    system("cls");
+
+    if (jugador > jugador14)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador14 << endl;
+        return(0);
+    }
+
+    ////////////////////////////////////////////////////
+
+
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Skye " << endl;
+    system("cls");
+
+    if (jugador > jugador15)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador15 << endl;
+        return(0);
+    }
+
+    ////////////////////////////////////////////////////
+
+
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Kayo " << endl;
+    system("cls");
+
+    if (jugador > jugador16)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador16 << endl;
+        return(0);
+    }
+
+    ////////////////////////////////////////////////////
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Omen " << endl;
+    system("cls");
+
+    if (jugador > jugador17)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador17 << endl;
+        return(0);
+    }
+
+    ////////////////////////////////////////////////////
+
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Brimmstone " << endl;
+    system("cls");
+
+    if (jugador > jugador18)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador18 << endl;
+        return(0);
+    }
+
+    ////////////////////////////////////////////////////
+
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Viper " << endl;
+    system("cls");
+
+    if (jugador > jugador19)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador19 << endl;
+        return(0);
+    }
+
+    ////////////////////////////////////////////////////
+
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs Harbor " << endl;
+    system("cls");
+
+    if (jugador > jugador20)
+        cout << "victoria" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador20 << endl;
+        return(0);
+    }
+
+
+    ////////////////////////////////////////////////////
+    system("cls");
+    cout << "que gane el mejor radiante " << endl;
+    cout << nickname << " vs joel el dios de valo " << endl;
+    system("cls");
+
+    if (jugador > jugador21)
+        cout << "victoria fulminante \n fin del royal" << endl;
+
+
+
+    else
+    {
+        cout << "haz muerto " << endl;
+        cout << jugador << " vs " << jugador21 << endl;
+        return(0);
+    }
+
+}
